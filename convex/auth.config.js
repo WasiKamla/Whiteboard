cfc07fe1d8124829ea4e17f373861728678d1c79
wasikamla <wasikamla00@gmail.com>
2024-02-01 10:17:19 +0500

    initial commit

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://just-mastodon-0.clerk.accounts.dev",
+      domain: "https://funky-jennet-38.clerk.accounts.dev",
       applicationID: "convex",
     },
-  ]
+  ],
 };
